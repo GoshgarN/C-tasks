2acdd94c729f69420a9a2e4bc4d8a4fa5237041d
GoshgarN <goshgarsn@code.edu.az>
2022-10-01 21:17:41 +0400

    add files to master

---
+++ prime and composite/prime and composite/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace prime_and_composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int number = 13;
+            int i;
+            int count = 0;
+
+            for (i = 1; i <= 50; i++) ;
+
+            {
+                if (13 % i == 0) ;
+                count++;
+
+            }
+            
+                if (count > 2) ;
+
+                Console.WriteLine("Composite number");
+               
+                Console.WriteLine("Prime number");
+
+            
+
+
+
+        }
+       
+        
+
+
+
+
+
+    }
+}
